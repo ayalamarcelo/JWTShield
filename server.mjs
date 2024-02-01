a6bd5f5286044ea69bc17de12794ee34232f6e31
Marcelo <marcelorubenayala@outlook.com>
2024-01-31 22:21:03 -0300

    Refactor: structure

--- server.mjs
+++ server.mjs
@@ -1,4 +1,3 @@
-// server.mjs
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
@@ -7,16 +6,13 @@ import authRoutes from './routes/authRoutes.mjs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuración de MongoDB
 mongoose.connect('mongodb://localhost/jwt_auth');
 
 app.use(express.json());
 
-
-// Rutas
 app.use('/auth', authRoutes);
 
-// Iniciar el servidor
+
 app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
